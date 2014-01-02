dcb90a6d0e946a60da44b75f27150edb27ef4ee7
akhilesh aggarwal <akhilesh_aggarwal@dell.com>
2014-01-02 06:37:59 +0000

    Puppet resource type for Nic Partitioning (npar) setting

---
+++ lib/puppet/provider/importnparsetting/default.rb
@@ -0,0 +1,80 @@
+provider_path = Pathname.new(__FILE__).parent.parent
+require 'rexml/document'
+include REXML
+require File.join(provider_path, 'idrac')
+require File.join(provider_path, 'checklcstatus')
+require File.join(provider_path, 'checkjdstatus')
+require File.join(provider_path, 'importtemplatexml')
+
+Puppet::Type.type(:importnparsetting).provide(:importnparsetting, :parent => Puppet::Provider::Idrac) do
+  desc "Dell idrac provider for  nic partitioning."
+  $count = 0
+  $maxcount = 30
+
+  def create
+    requiredstatus = resource[:status]
+    targetnic = resource[:nic]
+
+    file = File.new('/etc/puppet/modules/idrac/files/defaultxmls/nparsetting.xml')
+    xmldoc = Document.new(file)
+    
+    xmldoc.elements.each("SystemConfiguration/Component") {
+       |e|  e.attributes["FQDD"] = "#{targetnic}"
+    }
+
+    xmldoc.elements.each("SystemConfiguration/Component/Attribute") {
+        |e|  if e.attributes["Name"] == "NicPartitioning"
+                e.text = requiredstatus
+            end
+    }
+    
+    nparsettingfilename = "nparsetting_#{resource[:dracipaddress]}.xml"
+    nparsettingfilepath = "#{resource[:nfssharepath]}/#{nparsettingfilename}"
+    file = File.open("#{nparsettingfilepath}", "w")
+    xmldoc.write(file)
+    file.close
+
+    #Import System Configuration
+    obj = Puppet::Provider::Importtemplatexml.new(resource[:dracipaddress],resource[:dracusername],resource[:dracpassword],nparsettingfilename,resource[:nfsipaddress],resource[:nfssharepath])
+    instanceid = obj.importtemplatexml
+    puts "Instance id #{instanceid}"
+    for i in 0..30
+        obj = Puppet::Provider::Checkjdstatus.new(resource[:dracipaddress],resource[:dracusername],resource[:dracpassword],instanceid)
+        response = obj.checkjdstatus
+        puts "JD status : #{response}"
+        if response  == "Completed"
+            Puppet.info "Import System Configuration is completed."
+            break
+        else
+            Puppet.info "Job is running, wait for 1 minute"
+            sleep 60
+        end
+    end
+    if response != "Completed"
+      raise "Import System Configuration is still running."
+    end
+    
+
+  end
+  
+  def exists?
+    puts "Inside exist"
+    obj = Puppet::Provider::Checklcstatus.new(resource[:dracipaddress],resource[:dracusername],resource[:dracpassword])
+    response = obj.checklcstatus
+    response = response.to_i
+    if response == 0
+        return false
+    else
+        #recursive call  method exists till lcstatus =0
+        while $count < $maxcount  do
+            Puppet.info "LC status busy, wait for 1 minute"
+            sleep 60
+            $count +=1
+            exists?
+        end
+        raise Puppet::Error, "Life cycle controller is busy"
+        return true
+    end
+  end
+
+end
---
+++ lib/puppet/type/importnparsetting.rb
@@ -0,0 +1,67 @@
+#
+#  Enable/Disables Nic Partitioning on server
+#
+
+Puppet::Type.newtype(:importnparsetting) do
+    @doc = "Enable/Disable NIC partitioning on server."
+
+    ensurable do
+        newvalue(:present) do
+            provider.create
+        end
+     defaultto(:present)
+    end
+
+    newparam(:name) do
+        desc "The Namevar."
+        isnamevar
+        validate do |value|
+            unless value =~ /^\w+$/
+                raise "\'%s\' is not a valid title." % value
+            end
+        end
+
+    end
+
+    newparam(:nic) do
+      desc "The Target NIC where nic partitioning has to be modified."
+      isrequired
+      validate do |value|
+            unless value =~ /^NIC.Integrated/
+                raise "\'%s\' is not a valid nic name." % value
+            end
+        end
+    end
+    
+    newparam(:status) do
+        desc "The Required status of Nic Partitioning."
+        isrequired
+        newvalues("Enabled", "Disabled")
+        validate do |value|
+            unless value =~ /^\w+$/
+                raise "\'%s\' is not a valid nic partitioning status. Valid values are \"Enabled\" or \"Disabled\"" % value
+            end
+        end
+    end
+
+    newparam(:dracipaddress) do
+      desc "The Ip address of idrac."
+    end
+
+    newparam(:dracusername) do
+      desc "User name."
+    end
+
+    newparam(:dracpassword) do
+      desc "Password."
+    end
+
+    newparam(:nfsipaddress) do
+      desc "NFS Server ipaddress."
+    end
+
+    newparam(:nfssharepath) do
+      desc "NFS share path."
+    end
+
+end
