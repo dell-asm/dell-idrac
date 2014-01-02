296a7358d9c8016fefab0150b5da3414d966b669
akhilesh aggarwal <akhilesh_aggarwal@dell.com>
2014-01-02 08:43:46 +0000

    commented puts statements

--- lib/puppet/provider/checkjdstatus.rb
+++ lib/puppet/provider/checkjdstatus.rb
@@ -10,13 +10,13 @@ class Puppet::Provider::Checkjdstatus <  Puppet::Provider
   end
   def checkjdstatus
     #Get the job status
-    puts "InstanceID=#{@instanceid} -h #{@ip} -V -v -c dummy.cert -P 443 -u #{@username} -p #{@password} -j utf-8 -y basic"
+    #puts "InstanceID=#{@instanceid} -h #{@ip} -V -v -c dummy.cert -P 443 -u #{@username} -p #{@password} -j utf-8 -y basic"
     response = `wsman get "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_LifecycleJob?InstanceID=#{@instanceid}" -h #{@ip} -V -v -c dummy.cert -P 443 -u #{@username} -p #{@password} -j utf-8 -y basic`
     puts response
     xmldoc = Document.new(response)
     jdnode = XPath.first(xmldoc, "//n1:JobStatus")
     jdstatus=jdnode.text
-    puts "jd status #{jdstatus}"
+    #puts "JD status #{jdstatus}"
     return jdstatus
   end
 end
