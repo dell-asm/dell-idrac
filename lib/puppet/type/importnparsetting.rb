d19b97cdc8afad18013f06547fb2eea04694fb1b
akhilesh aggarwal <akhilesh_aggarwal@dell.com>
2014-01-02 10:08:16 +0000

    added validation

--- lib/puppet/type/importnparsetting.rb
+++ lib/puppet/type/importnparsetting.rb
@@ -37,31 +37,59 @@ Puppet::Type.newtype(:importnparsetting) do
         desc "The Required status of Nic Partitioning."
         isrequired
         newvalues("Enabled", "Disabled")
-        validate do |value|
-            unless value =~ /^\w+$/
-                raise "\'%s\' is not a valid nic partitioning status. Valid values are \"Enabled\" or \"Disabled\"" % value
+        munge do |value|
+            if value.strip.length == 0
+                value.to_s
+            else
+                value.to_s
             end
         end
+
     end
 
     newparam(:dracipaddress) do
       desc "The Ip address of idrac."
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "Server drac Ip address must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:dracusername) do
       desc "User name."
+ 	  validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "Server drac username must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:dracpassword) do
       desc "Password."
+ 	  validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "Server drac password must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:nfsipaddress) do
       desc "NFS Server ipaddress."
+ 	  validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The NFS share ipaddress must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:nfssharepath) do
-      desc "NFS share path."
+      desc "NFS share path." 
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The NFS sharepath must contain a value. It cannot be null."
+        end
+      end
     end
 
 end
