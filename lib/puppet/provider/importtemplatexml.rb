66b3a7fae7c71aa4048a7443db61c6490c0dc2f9
BISWAJIT RAUT <BISWAJIT_RAUT@dell.com>
2014-01-03 10:52:33 +0000

    Error handling

--- lib/puppet/provider/importtemplatexml.rb
+++ lib/puppet/provider/importtemplatexml.rb
@@ -14,10 +14,14 @@ class Puppet::Provider::Importtemplatexml <  Puppet::Provider
   def importtemplatexml
     puts "#{@ip} -V -v -c dummy.cert -P 443 -u #{@username} -p #{@password} -a ImportSystemConfiguration -k IPAddress=#{@nfsipaddress} -k ShareName=#{@nfssharepath}  -k FileName=#{@configxmlfilename}"
     response = `wsman invoke http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_LCService?SystemCreationClassName="DCIM_ComputerSystem",CreationClassName="DCIM_LCService",SystemName="DCIM:ComputerSystem",Name="DCIM:LCService" -h #{@ip} -V -v -c dummy.cert -P 443 -u #{@username} -p #{@password} -a ImportSystemConfiguration -k "IPAddress=#{@nfsipaddress}" -k "ShareName=#{@nfssharepath}" -k "ShareType=0" -k "FileName=#{@configxmlfilename}"`
-    puts response
+    Puppet.info "#{response}"
     # get instance id
     xmldoc = Document.new(response)
     instancenode = XPath.first(xmldoc, '//wsman:Selector Name="InstanceID"')
+	tempinstancenode = instancenode
+    if tempinstancenode.to_s == ""
+      raise "Job ID not created"
+    end
     instanceid=instancenode.text
     puts "Instance id #{instanceid}"
     return instanceid
