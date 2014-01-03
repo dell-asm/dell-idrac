a413bc23e2601ecc6a141d4f842b57e84a079d9b
awadhesh gupta <awadhesh_gupta@dell.com>
2014-01-03 12:24:22 +0000

    Added reboot modules

---
+++ lib/puppet/provider/reboot.rb
@@ -0,0 +1,22 @@
+require 'rexml/document'
+include REXML
+
+class Puppet::Provider::Reboot <  Puppet::Provider
+  def initialize (ip,username,password,rebootfilepath)
+    @ip = ip
+    @username = username
+    @password = password
+    @rebootfilepath = rebootfilepath
+  end
+
+  def reboot
+    response = `wsman invoke -a CreateTargetedConfigJob http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_RAIDService?SystemCreationClassName=DCIM_ComputerSystem,CreationClassName=DCIM_RAIDService,SystemName=DCIM:ComputerSystem,Name=DCIM:RAIDService -h #{@ip} -V -v -c dummy.cert -P 443 -u #{@username} -p #{@password} -J #{@rebootfilepath} -j utf-8 -y basic`
+    puts response
+    # get instance id
+    xmldoc = Document.new(response)
+    instancenode = XPath.first(xmldoc, '//wsman:Selector Name="InstanceID"')
+    instanceid=instancenode.text
+    puts "Instance id #{instanceid}"
+    return instanceid
+  end  
+end
