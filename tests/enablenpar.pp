7ef5789e12200defd41ac79c3ea61e55045d5871
sharninder khera <sharninder_khera@dell.com>
2014-01-03 09:38:29 +0000

    Added sample pp files

---
+++ tests/enablenpar.pp
@@ -0,0 +1,11 @@
+importnparsetting { 
+  'nicapplyconfig':
+	  nic => 'NIC.Integrated.1-1-1',
+		status => 'Enabled',
+		dracipaddress => '172.17.15.109',
+		dracusername => 'root',
+		dracpassword => 'calvin',
+		nfsipaddress => '172.28.15.192',
+		nfssharepath => '/var/lib/nfs',
+}
+
