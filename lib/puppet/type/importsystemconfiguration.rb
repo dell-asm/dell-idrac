0a34ec77e7aba82170689e942246ae3c5c72eb9e
awadhesh gupta <awadhesh_gupta@dell.com>
2014-01-03 05:05:57 +0000

    Updated biosconfiguration script

--- lib/puppet/type/importsystemconfiguration.rb
+++ lib/puppet/type/importsystemconfiguration.rb
@@ -14,29 +14,93 @@ Puppet::Type.newtype(:importsystemconfiguration) do
 
     newparam(:name) do
       desc "The test name."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The name must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:dracipaddress) do
       desc "The Ip address of idrac."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The dracipaddress must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:dracusername) do
       desc "User name."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The dracusername must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:dracpassword) do
       desc "Password."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The dracpassword must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:configxmlfilename) do
       desc "Config Xml file name."
-    end
+      munge do |value|
+        value.to_s
+      end
 
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The Config Xml file name must contain a value. It cannot be null."
+        end
+      end
+    end
+    
     newparam(:nfsipaddress) do
       desc "NFS Server ipaddress."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The nfsipaddress must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:nfssharepath) do
       desc "NFS share path."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The nfssharepath must contain a value. It cannot be null."
+        end
+      end
     end
+
 end
