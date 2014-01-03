d7edbba09cd33cebade9c1ca81ae2f49493d42d1
awadhesh gupta <awadhesh_gupta@dell.com>
2014-01-03 05:00:21 +0000

    Updated biosconfiguration script

--- lib/puppet/type/importbiosconfiguration.rb
+++ lib/puppet/type/importbiosconfiguration.rb
@@ -14,26 +14,80 @@ Puppet::Type.newtype(:importbiosconfiguration) do
 
     newparam(:name) do
       desc "The test name."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The name must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:dracipaddress) do
       desc "The Ip address of idrac."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The dracipaddress must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:dracusername) do
       desc "User name."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The dracusername must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:dracpassword) do
       desc "Password."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The dracpassword must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:nfsipaddress) do
       desc "NFS Server ipaddress."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The nfsipaddress must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:nfssharepath) do
       desc "NFS share path."
+      munge do |value|
+        value.to_s
+      end
+
+      validate do |value|
+        if value.strip.length == 0
+          raise ArgumentError, "The nfssharepath must contain a value. It cannot be null."
+        end
+      end
     end
 
     newparam(:memtest) do
@@ -166,6 +220,18 @@ Puppet::Type.newtype(:importbiosconfiguration) do
       desc "IntegratedNetwork1 is Enable or Disable."
       newvalues("Enabled", "Disabled")
       defaultto("Enabled")
+      munge do |value|
+        if value.strip.length == 0
+          value.to_s
+        else
+          value.to_s
+        end
+      end
+    end
+
+    newparam(:biosbootseq) do
+      desc "BiosBootSeq"
+      defaultto("HardDisk.List.1-1, NIC.Integrated.1-1-1, NIC.Integrated.1-2-1")
       munge do |value|
         if value.strip.length == 0
           value.to_s
