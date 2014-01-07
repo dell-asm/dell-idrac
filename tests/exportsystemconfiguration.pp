780ffe4bc716a49321d8436efeaedd104b5b4bdf
awadhesh gupta <awadhesh_gupta@dell.com>
2014-01-07 10:55:52 +0000

    Added export system configuration manifest

---
+++ tests/exportsystemconfiguration.pp
@@ -0,0 +1,9 @@
+exportsystemconfiguration { 'exportsystemconfiguration':
+  dracipaddress => '172.17.10.106',
+  dracusername => 'root',
+  dracpassword => 'calvin',
+  configxmlfilename => '123.txt',
+  nfsipaddress => '172.28.10.189',
+  nfssharepath => '/root/Test_sysConf',
+}
+
