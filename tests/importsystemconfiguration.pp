7d722d976444289d31edd28640bb2d46aa04896a
Sharninder <sharninder_khera@dell.com>
2014-01-22 15:00:53 +0530

    Some cosmetic changes. Changed nfs path and xml filename being shipped with the sample manifests

--- tests/importsystemconfiguration.pp
+++ tests/importsystemconfiguration.pp
@@ -2,8 +2,8 @@ importsystemconfiguration { 'importsystemconfiguration':
   dracipaddress => '172.17.10.106',
   dracusername => 'root',
   dracpassword => 'calvin',
-  configxmlfilename => '123.txt',
+  configxmlfilename => 'testconfig.xml',
   nfsipaddress => '172.28.10.189',
-  nfssharepath => '/root/biswajeet',
+  nfssharepath => '/var/nfs',
 }
 
