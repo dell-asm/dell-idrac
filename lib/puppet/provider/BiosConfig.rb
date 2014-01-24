dc12e9971e6eb3da419822fc0f76076fb8c3b629
Awadhesh_Gupta <Awadhesh_Gupta@dell.com>
2014-01-24 20:35:49 +0530

    Changes for bios boot order configuration (ASM-489)

---
+++ lib/puppet/provider/BiosConfig.rb
@@ -0,0 +1,148 @@
+require 'rexml/parsers/treeparser'
+
+provider_path = Pathname.new(__FILE__).parent
+require 'rexml/document'
+
+include REXML
+require File.join(provider_path, 'idrac')
+require File.join(provider_path, 'checklcstatus')
+require File.join(provider_path, 'checkjdstatus')
+require File.join(provider_path, 'reboot')
+
+class Puppet::Provider::BiosConfig <  Puppet::Provider
+  def initialize (ip,username,password,boottype)
+    @ip = ip
+    @username = username
+    @password = password
+    @boottype  = boottype
+    @instid = ""
+
+  end
+
+  def GetBootSourceSetting
+    xmlresp= wsmancmd "bootorder"
+    puts xmlresp
+    if (xmlresp !="")
+      varbootseq=  parseResponse xmlresp
+      puts "varbootseq : #{varbootseq}"
+      if (varbootseq .length >0)
+        retval=  creatBootSeq varbootseq
+        if retval == true
+          resp=wsmancmd "updatebootorder"
+          puts "Response : #{resp}"
+          # get response msg
+          xmldoc = Document.new(resp)
+          msg = XPath.first(xmldoc, '//n1:Message')
+          tempmsg = msg
+          if tempmsg.to_s == ""
+            raise "Failed to update boot order Sequence"
+          end
+          msg=msg.text
+          puts "MESSAGE : #{msg}"
+          if msg =~ /Success/i
+            resp= wsmancmd "changebootsource"
+            puts resp
+            createJobid
+          end
+        end
+      end
+    end
+  end
+
+  def createJobid
+    #Reboot
+    instanceid = rebootinstanse
+    if instanceid == "success"
+     return 
+    end
+    puts "instanceid : #{instanceid}"
+    Puppet.info "instanceid : #{instanceid}"
+    for i in 0..30
+      response = checkjobstatus instanceid
+      Puppet.info "JD status : #{response}"
+      if response  == "Completed" || response == "Reboot Completed"
+        Puppet.info "Boot order Sequence is completed."
+        break
+      else
+        if response  == "Failed"
+          raise "Job ID is not created."
+        else
+          Puppet.info "Job is running, wait for 1 minute"
+          sleep 60
+        end
+      end
+    end
+    if response != "Completed" && response != "Reboot Completed"
+      raise "Boot order Sequence is still running."
+    end
+  end
+
+  def wsmancmd(cmd)
+    resp=""
+    if cmd =="bootorder" then
+      resp = `wsman enumerate http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_BootSourceSetting -h #{@ip}  -V -v -c dummy.cert -P 443 -u #{@username} -p #{@password} -j utf-8 -y basic`
+      return   resp
+    end
+    if cmd == "updatebootorder"
+      resp = `wsman invoke -a ChangeBootOrderByInstanceID http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_BootConfigSetting?InstanceID=IPL -h #{@ip} -V -v -c dummy.cert -P 443 -u #{@username} -p #{@password} -J /tmp/ChangeBootOrder_#{@ip}.xml -j utf-8 -y basic`
+      return   resp
+    end
+    if cmd == "changebootsource"
+      resp = `wsman invoke -a ChangeBootSourceState http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_BootConfigSetting?InstanceID=IPL -h #{@ip} -V -v -c dummy.cert -P 443 -u #{@username} -p #{@password} -k EnabledState=0 -k source=#{@instid} -j utf-8 -y basic`
+      return   resp
+    end
+    
+  end
+
+  def parseResponse(xmldata)
+    ary= Array.new
+    data=xmldata.split(/\n/)
+    data.each do |ele|
+      if ele.to_s =~ /<n1:InstanceID>(\S+)<\/n1:InstanceID>/
+        vardata = $1
+        if vardata =~ /\#BootSeq\#/
+          @instid = vardata
+          ary.push(vardata)
+        end
+      end
+    end
+    return ary
+  end
+
+  def creatBootSeq(ary)
+    _flag =false
+    bootorderfilepath = File.join(Pathname.new(__FILE__).parent.parent.parent.parent, 'files/defaultxmls/ChangeBootOrder.xml')
+    file = File.new(bootorderfilepath)
+    xmldoc = Document.new(file)
+    xmlroot = xmldoc.root
+    ary.each do |val|
+      if val.to_s =~ /#{@boottype}/i
+        instanceID = val
+        pdarray = Element.new "p:source"
+        pdarray.text = "#{instanceID}"
+        xmlroot.add_element pdarray
+      end
+    end
+    file = File.open("/tmp/ChangeBootOrder_#{@ip}.xml", "w")
+    xmldoc.write(file)
+    file.close
+    _flag= true
+    return _flag
+  end
+
+  def rebootinstanse
+    #Reboot
+    rebootfilepath = File.join(Pathname.new(__FILE__).parent.parent.parent.parent, 'files/defaultxmls/rebootidrac.xml')
+    obj = Puppet::Provider::Reboot.new(@ip,@username,@password,rebootfilepath)
+    instanceid = obj.rebootidrac
+    puts "instanceid : #{instanceid}"
+    return instanceid
+  end
+
+  def checkjobstatus(instanceid)
+    obj = Puppet::Provider::Checkjdstatus.new(@ip,@username,@password,instanceid)
+    response = obj.checkjdstatus
+  end
+
+end
+
--- lib/puppet/provider/reboot.rb
+++ lib/puppet/provider/reboot.rb
@@ -11,7 +11,7 @@ class Puppet::Provider::Reboot <  Puppet::Provider
   end
 
   def reboot
-	  response = executerebootcmd
+    response = executerebootcmd
     Puppet.info "#{response}"
     # get instance id
     xmldoc = Document.new(response)
@@ -25,8 +25,28 @@ class Puppet::Provider::Reboot <  Puppet::Provider
     return instanceid
   end
 
+  def rebootidrac
+    # Create the reboot job
+    puts "Rebooting server #{@ip}"
+    #response = `wsman invoke -a CreateRebootJob http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_SoftwareInstallationService?CreationClassName=DCIM_SoftwareInstallationService,SystemCreationClassName=DCIM_ComputerSystem,SystemName=IDRAC:ID,Name=SoftwareUpdate -h "#{@ip}" -V -v -c dummy.cert -P 443 -u "#{@username}" -p "#{@password}" -J #{@rebootfilepath} -j utf-8 -y basic -k "RebootJobType=2" -k "ShutdownType=1"`
+    response = `wsman invoke -a CreateTargetedConfigJob http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_BIOSService?SystemCreationClassName=DCIM_ComputerSystem,CreationClassName=DCIM_BIOSService,SystemName=DCIM:ComputerSystem,Name=DCIM:BIOSService -h "#{@ip}" -V -v -c dummy.cert -P 443 -u "#{@username}" -p "#{@password}" -J #{@rebootfilepath} -j utf-8 -y basic`
+    xmldoc = Document.new(response)
+    instancenode = XPath.first(xmldoc, '//wsman:Selector Name="InstanceID"')
+    tempinstancenode = instancenode
+    if tempinstancenode.to_s == ""
+      msgnode = XPath.first(xmldoc, '//n1:Message')
+      if msgnode.to_s =~ /No pending data present to create a Configuration job/
+        return "success"
+      end
+      raise "Job ID not created"
+    end
+    instanceid=instancenode.text
+    puts "instanceid #{instanceid}"
+    return instanceid
+  end
+
   def executerebootcmd
-	  resp = `wsman invoke -a CreateTargetedConfigJob http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_RAIDService?SystemCreationClassName=DCIM_ComputerSystem,CreationClassName=DCIM_RAIDService,SystemName=DCIM:ComputerSystem,Name=DCIM:RAIDService -h #{@ip} -V -v -c dummy.cert -P 443 -u #{@username} -p #{@password} -J #{@rebootfilepath} -j utf-8 -y basic`
-	  return resp
+    resp = `wsman invoke -a CreateTargetedConfigJob http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/root/dcim/DCIM_RAIDService?SystemCreationClassName=DCIM_ComputerSystem,CreationClassName=DCIM_RAIDService,SystemName=DCIM:ComputerSystem,Name=DCIM:RAIDService -h #{@ip} -V -v -c dummy.cert -P 443 -u #{@username} -p #{@password} -J #{@rebootfilepath} -j utf-8 -y basic`
+    return resp
   end
 end
