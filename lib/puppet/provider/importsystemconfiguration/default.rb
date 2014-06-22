89c9b58eb2629be03448c02c464f1598b00ac171
Sushil Rai <Sushil_R@Dell.com>
2014-06-21 21:44:23 -0500

    iDRAC reset in case import failure
    Added a iDRAC reset in case the import configuration fails for the first time.
    Before performing the second attempt of the import configuration, iDRAC reset, LC check, server reboot and another export configuration is performed.

--- lib/puppet/provider/importsystemconfiguration/default.rb
+++ lib/puppet/provider/importsystemconfiguration/default.rb
@@ -11,6 +11,49 @@ Puppet::Type.type(:importsystemconfiguration).provide(
   desc "Dell idrac provider for import system configuration."
 
   def create
+    import_try = 1
+    instanceid = importtemplate
+    Puppet.info "Instance id #{instanceid}"
+    for i in 0..30
+      response = checkjobstatus instanceid
+      Puppet.info "JD status : #{response}"
+      if response  == "Completed"
+        Puppet.info "Import System Configuration is completed."
+        break
+      else
+        if response  == "Failed"
+          if import_try == 1
+            return retry_import
+          else   
+            raise "Job Failed ."
+          end
+        else
+          Puppet.info "Job is running, wait for 1 minute"
+          sleep 60
+        end
+      end
+    end
+    if response != "Completed"
+      raise "Import System Configuration is still running."
+    end
+  end
+  
+  def retry_import
+    Puppet.debug("Import operation failed in the first attempt, retrying import operation")
+    Puppet.debug("Resetting the iDRAC before performing any other operation")
+    reset
+    Puppet.debug("Waiting for Lifecycle Controller be ready")
+    lcstatus
+    reboot
+    lcstatus
+    exporttemplate
+    
+    synced = !resource[:force_reboot] && config_in_sync?
+    if synced
+      Puppet.debug("Configuration are already in synced, skipping the import operation")
+      return true
+    end
+        
     instanceid = importtemplate
     Puppet.info "Instance id #{instanceid}"
     for i in 0..30
