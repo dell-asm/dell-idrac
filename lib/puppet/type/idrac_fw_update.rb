e1122bfa08f464096fb193cd7518e12009e94e65
logan kimmel <logan@lkimmel.com>
2014-09-09 16:24:12 +0000

    Spelling
    Fix spelling mistakes

--- lib/puppet/type/idrac_fw_update.rb
+++ lib/puppet/type/idrac_fw_update.rb
@@ -9,18 +9,13 @@ Puppet::Type.newtype(:idrac_fw_update) do
 
   newparam(:asm_hostname) do
     desc "The ip address for the remote location of the firmware"
-    validate do |value|
-      unless value =~ /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
-        raise ArgumentError, "%s is not a valid IP Address" % value
-      end
-    end
   end
 
   newparam(:path) do
-    desc "The path to the remote location of the firmwre (on the network share)"
+    desc "The path to the remote location of the firmware (on the network share)"
     validate do |value|
       unless File.directory? value
-        raise ArgumentErrorm, "The path: %x does not exist" % value
+        raise ArgumentError, "The path: %x does not exist" % value
       end
     end
   end
