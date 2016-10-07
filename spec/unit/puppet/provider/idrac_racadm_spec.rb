21d81d1e14547ac7375ae0b7f7714fd6276a4e42
kaushalgala <kaushal_gala@dell.com>
2016-10-06 19:55:00 -0500

    ASM-7905 Puppet provider for server onboarding
    Implementation for new type and provider for configuring credentials
    and network settings for a given server using racadm commands. We keep
    same style of implementation as we did for blades in dell-cmc module,
    except for the fact the racadm calls here go directly to the server.
    We also clean up and yard doc some execution methods in racadm.rb
    
    Note: Maybe we should later refactor the code to move racadm command
    to a common project where eventually all racadm calls for servers go to
    single place. Right now some code is unnecessarily duplicated between
    racadm.rb in dell-cmc and dell-idrac modules.

---
+++ spec/unit/puppet/provider/idrac_racadm_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper_ext"
+require "rspec/expectations"
+require "puppet/provider/idrac_racadm"
+
+describe Puppet::Provider::IdracRacadm do
+  let(:racadm) { Puppet::Provider::IdracRacadm.new }
+  let(:ssh) { Object.new }
+
+  context "instance validation" do
+    it "should have instance object" do
+      expect(racadm.class).to eq(Puppet::Provider::IdracRacadm)
+    end
+  end
+
+  context "#racadm_cmd" do
+    it "should execute fine for successful command having params" do
+      ssh.stubs(:exec!).with("racadm cmd foo bar").returns("Object executed successfully")
+      racadm.stubs(:client).returns(ssh)
+      expect(racadm.racadm_cmd("cmd", ["foo", "bar"] )).to eq("Object executed successfully")
+    end
+
+    it "should fail when command errors and raise on err is specified" do
+      ssh.stubs(:exec!).returns("ERROR: Something went wrong")
+      racadm.stubs(:client).returns(ssh)
+      expect {racadm.racadm_cmd("cmd", [], :raise_on_err => true)}.to raise_error(/Error in racadm command/)
+    end
+
+    it "should return an array for command that returns multi-line output" do
+      ssh.stubs(:exec!).returns("Result\nGood")
+      racadm.stubs(:client).returns(ssh)
+      expect(racadm.racadm_cmd("cmd", [], :raise_on_err => true)).to eq(["Result", "Good"])
+    end
+  end
+end
---
+++ spec/unit/puppet/provider/server_onboard/server_onboard_spec.rb
@@ -0,0 +1,91 @@
+require "spec_helper_ext"
+require "rspec/expectations"
+
+describe Puppet::Type.type(:server_onboard).provider(:default) do
+  let(:resource) { Puppet::Type.type(:server_onboard).new({ :name => "rackserver-ABCD123"}) }
+
+  let(:provider) {resource.provider}
+
+  context "instance validation" do
+    it "should have instance object" do
+      expect(provider.class).to eq(Puppet::Type::Server_onboard::ProviderDefault)
+    end
+  end
+
+  context "set credential" do
+    it "should raise error when credentials are not set" do
+      provider.resource[:credential] = {}
+      expect {provider.setup_credential}.to raise_error(/not specified/)
+    end
+
+    it "should invoke racadm_set commands for specified credentials" do
+      provider.stubs(:racadm_set).with(anything()).returns("Object Executed Successfully")
+      provider.expects(:racadm_set).times(2)
+      provider.resource[:credential] = {"username" => "foo", "password" => "bar"}
+      provider.setup_credential
+    end
+  end
+
+  context "set network type" do
+    before(:each) do
+      provider.stubs(:config_static_network).with(anything())
+    end
+
+    it "should not invoke static config when not specified" do
+      provider.expects(:config_static_network).never
+      provider.resource[:network_type] = "existing"
+      provider.setup_network
+    end
+
+    it "should invoke static config when specified" do
+      provider.expects(:config_static_network).once
+      provider.resource[:networks] = {
+        "staticNetworkConfiguration" => {
+          "ipAddress" => "1.2.3.4", "subnet" => "1.1.0.0", "gateway" => "1.2.0.1" }
+      }
+      provider.resource[:network_type] = "static"
+      provider.setup_network
+    end
+
+    it "should raise error when static type specified without config" do
+      provider.expects(:config_static_network).never
+      provider.resource[:networks] = {}
+      provider.resource[:network_type] = "static"
+      expect {provider.setup_network}.to raise_error(/not static/)
+    end
+  end
+
+  context "set static configuration" do
+    before(:each) do
+      provider.stubs(:racadm_set).with(anything()).returns("Object Executed Successfully")
+      provider.stubs(:racadm_cmd).with(anything()).returns("Object Executed Successfully")
+      provider.stubs(:wait_for_ip).with(anything())
+    end
+
+    it "should set only static IP and not dns configs" do
+      provider.expects(:racadm_set).never
+      provider.expects(:racadm_cmd).once
+      provider.expects(:wait_for_ip).once
+      provider.config_static_network("ipAddress" => "1.2.3.4", "subnet" => "1.1.0.0", "gateway" => "1.2.0.1")
+    end
+
+    it "should set static IP and primary dns" do
+      provider.expects(:racadm_set).once
+      provider.expects(:racadm_cmd).once
+      provider.expects(:wait_for_ip).once
+      provider.config_static_network(
+        "ipAddress" => "1.2.3.4", "subnet" => "1.1.0.0", "gateway" => "1.2.0.1", "primaryDns" => "4.5.6.7"
+      )
+    end
+
+    it "should set static IP and all dsn configs" do
+      provider.expects(:racadm_set).times(2)
+      provider.expects(:racadm_cmd).once
+      provider.expects(:wait_for_ip).once
+      provider.config_static_network(
+        "ipAddress" => "1.2.3.4", "subnet" => "1.1.0.0", "gateway" => "1.2.0.1",
+        "primaryDns" => "4.5.6.7", "secondaryDns" => "8.9.10.11"
+      )
+    end
+  end
+end
